e08d55cea318a6178dabdaa360faad08586b7c0c
VANSH SHARMA <vanshsharma8750@gmail.com>
2024-10-27 17:31:30 +0530

    Add files via upload

---
+++ ConnectionClass.java
@@ -0,0 +1,24 @@
+package ams;
+
+import java.sql.*;
+
+public class ConnectionClass {
+    Connection con;
+    Statement stm;
+
+    ConnectionClass(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/ams","root","Vansh@2604");
+            stm=con.createStatement();
+
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new ConnectionClass();
+    }
+}
